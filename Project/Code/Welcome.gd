e973dad6cce4b522aecd35038ecf069f5cc30066
MasterKitten <Stevenson2628@gmail.com>
2023-02-10 20:19:36 -0800

    Started working on this.
    Up next: finishing this with a small quiz, small save? & flash card template.

--- Project/Code/Welcome.gd
+++ Project/Code/Welcome.gd
@@ -1,11 +1,27 @@
-extends Control
+extends Node
 
+@export_node_path("Button") var Continue
+@export_node_path("RichTextLabel") var Text
+@export var words: PackedStringArray
 
-# Called when the node enters the scene tree for the first time.
+@onready var Animator = get_node("AnimationPlayer")
+
+var current_array = 0
+
+# start by having the text appear
 func _ready():
-	pass # Replace with function body.
+	get_node(Text).set_text(words[current_array])
+	Animator.play("appear")
 
+# Makes the text go bye bye
+func _on_agree_button_pressed():
+	Animator.play("disappear")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+# Once the text is done being textual, start the short quiz.
+func Change_Text():
+	current_array += 1
+	if words.size() < current_array - 1:
+		get_node(Text).set_text(words[current_array])
+		Animator.play("appear")
+	else:
+		print("done!")
