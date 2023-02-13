19d63438c4376d6358e6ee913546e5887d8e10a1
Cataclock <Stevenson2628@gmail.com>
2023-02-13 08:22:04 -0800

    Seperated the main menu from the flash card, started on writing prompt

--- Project/Code/studying_part.gd
+++ Project/Code/studying_part.gd
@@ -5,8 +5,10 @@ func _on_back_button_pressed():
 
 var number = 0
 var selected_number = 0
+
 var something_selected = false
 @onready var Lock_in_button = get_node("Menu Menu/Select Set")
+
 @onready var menu_animation = get_node("Menu Menu/AnimationPlayer")
 
 # This might be complicated, so lets take it slow.
@@ -30,61 +32,11 @@ func _ready():
 		number += 1
 	menu_animation.play("appear")
 
-# All the code below is regarding flash card variables and the select script to go to flash cards
-var terms: PackedStringArray
-var description: PackedStringArray
+signal show_flash_card
 
-var term_number = 0
-@onready var term_object = get_node("Flash Card/ColorRect/CenterContainer/Label")
-var flipped = false
+signal show_writing_prompt
 
-@onready var card_animator = get_node("Flash Card/AnimationPlayer")
 # TO DO: MAKE THE SH## DISAPPEAR SO IT LOOKS GOOD.
 func _on_select_set_pressed():
-	var file = FileAccess.open("res://Set" + str(selected_number) + ".txt", FileAccess.READ)
-	get_node("Flash Card/Name").set("text", file.get_line())
-	# Set visibility of stuff
+	emit_signal("show_flash_card", selected_number)
 	get_node("Menu Menu").set("visible", false)
-	get_node("Flash Card").set("visible", true)
-	# Now add those lovely terms onto that array
-	var next_line = file.get_line()
-	while next_line != "":
-		terms.append(next_line)
-		next_line = file.get_line()
-		if next_line == "":
-			description.append("null")
-		else:
-			description.append(next_line)
-		next_line = file.get_line()
-	term_object.set("text", terms[0])
-# All the code below is regarding flash cards
-func _on_left_pressed():
-	if card_animator.is_playing():
-		return
-	term_number -= 1
-	if term_number < 0:
-		term_number = terms.size() - 1
-	term_object.set("text", terms[term_number])
-	flipped = false
-	card_animator.play("Left")
-
-func _on_right_pressed():
-	if card_animator.is_playing():
-		return
-	term_number += 1
-	if term_number > terms.size() - 1:
-		term_number = 0
-	term_object.set("text", terms[term_number])
-	flipped = false
-	card_animator.play("Right")
-
-func _on_flip_pressed():
-	if !card_animator.is_playing():
-		card_animator.play("Flip")
-
-func flip():
-	if flipped == false:
-		term_object.set("text", description[term_number])
-	else:
-		term_object.set("text", terms[term_number])
-	flipped = !flipped
