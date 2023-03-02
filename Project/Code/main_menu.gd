64393bfbabf8d994e351b7f6a9afa53940adc4a6
Cataclock <Stevenson2628@gmail.com>
2023-03-02 13:26:34 -0800

    New Daily rewards system. Continuing later...

--- Project/Code/main_menu.gd
+++ Project/Code/main_menu.gd
@@ -4,10 +4,24 @@ extends Control
 var scene_empty = false
 var button_number = 0
 
+@onready var Date = Time.get_date_dict_from_system()
+
 func _ready():
 	if !FileAccess.file_exists("res://settings.txt"):
 		get_tree().change_scene_to_file("res://Scene/welcome.tscn")
 	Animator.play("GetIn")
+	
+	if FileAccess.file_exists("res://Date.txt"):
+		var file = FileAccess.open("res://Date.txt", FileAccess.READ)
+		var day = file.get_line()
+		if (Date.day > int(day)) || (Date.day == 0 && int(day) != 0):
+			print("Hey, give this man a daily award!")
+			file = null
+			file = FileAccess.open("res://Date.txt", FileAccess.WRITE)
+			file.store_line(str(Date.day))
+	else:
+		var file = FileAccess.open("res://Date.txt", FileAccess.WRITE)
+		file.store_line(str(Date.day))
 
 func button(number: int):
 	button_number = number
